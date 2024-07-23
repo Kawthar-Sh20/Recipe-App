bb09d077e463d4dd71ac558301039d347d04e560
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-23 16:42:51 +0300

    Adding CORS Headers

--- Backend/api/insertRecipe.php
+++ Backend/api/insertRecipe.php
@@ -1,6 +1,9 @@
 <?php
 
 require "../connection.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $data = json_decode(file_get_contents("php://input"), true);
