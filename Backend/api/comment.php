5eecb501fb51c8475d0c3176b3ebd29a81086165
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-23 16:55:02 +0300

    Adding Cors Headers

--- Backend/api/comment.php
+++ Backend/api/comment.php
@@ -1,6 +1,10 @@
 <?php
 
 require "../connection.php";
+header("Access-Control-Allow-Origin: *"); // Allow all origins. Change "*" to your specific frontend URL if needed.
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $data = json_decode(file_get_contents("php://input"), true);
